521f7fa9c9aa90a5ed05d2b7e45d97aebefd6c91
藤井　晃太郎 <171x018x@istc-c-019.epc.center.kobe-u.ac.jp>
2017-04-17 16:04:33 +0900

    Initial commit

---
+++ vec3.js
@@ -0,0 +1,55 @@
+// Constructor
+Vec3 = function(x,y,z)
+{
+    this.x = x;
+    this.y = y;
+    this.z = z;
+}
+//Add method
+Vec3.prototype.add = function(v)
+{
+    this.x += v.x;
+    this.y += v.y;
+    this.z += v.z;
+    return this;
+}
+//Sum method
+Vec3.prototype.sum = function()
+{
+    return this.x + this.y + this.z;
+}
+//min method
+Vec3.prototype.min = function()
+{
+    var u =x;
+    if(x>y){
+	u=y;
+    }
+    if(y>z){
+	u=z;
+    }
+    if(x>z){
+	u=z
+    }
+    return u;
+}
+//Max method
+Vec3.prototype.min = function()
+{
+    var u =x;
+    if(x<y){
+	u=y;
+    }
+    if(y<z){
+	u=z;
+    }
+    if(x<z){
+	u=z
+    }
+    return u;
+}
+//mid method
+Vec3.prototype.mid = function()
+{
+    return x+y+z-this.min-this.max
+}
